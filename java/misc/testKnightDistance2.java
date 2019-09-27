5856c715ed36a7e0cbf062847d62ba2333ba4cdd
DinhLantstk789 <37432041+DinhLantstk789@users.noreply.github.com>
2019-09-27 10:49:03 +0700

    Add files via upload

---
+++ java/misc/testKnightDistance2.java
@@ -0,0 +1,50 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package testKnightDistance;
+
+import misc.KnightDistance;
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author black
+ */
+public class testKnightDistance {
+    
+    @Test
+    public void Test1(){
+        KnightDistance p = new KnightDistance();
+        assertEquals(p.dist(2,1,6,1), 2);
+    }
+    
+    @Test
+    public void Test2(){
+        KnightDistance p = new KnightDistance();
+        assertEquals(p.dist(1,2,1,6), 2);
+    }
+    
+    @Test
+    public void Test3(){
+        KnightDistance p = new KnightDistance();
+        assertEquals(p.dist(1,2,1,7), 3);
+    }
+    
+    @Test
+    public void Test4(){
+        KnightDistance p = new KnightDistance();
+        assertEquals(p.dist(1,2,1,8), 4);
+    }
+    // TODO add test methods here.
+    // The methods must be annotated with annotation @Test. For example:
+    //
+    // @Test
+    // public void hello() {}
+}
